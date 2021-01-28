00bf7f9c6ad2d963c3a654105640b0a0d549ae50
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-28 15:00:04 +0530

    Added code to detect scroll in calendar component.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import Calendar from "./Components/pages/Calendar";
 
--- src/Components/pages/Calendar.js
+++ src/Components/pages/Calendar.js
@@ -2,9 +2,29 @@ import React, { Component } from "react";
 import Box from "../common/Box";
 
 export class Calendar extends Component {
+    constructor(props) {
+        super(props);
+        this.handleScroll = this.handleScroll.bind(this);
+    }
+    componentDidMount() {
+        this.prev = window.scrollY;
+        window.addEventListener("wheel", e => this.handleScroll(e));
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener("wheel", this.handleScroll);
+    }
+
+    handleScroll(e) {
+        if (e.deltaY < 0) {
+            console.log("scrolling up");
+        } else if (e.deltaY > 0) {
+            console.log("scrolling down");
+        }
+    }
     render() {
         return (
-            <div>
+            <div onScroll={this.handleScroll}>
                 <Box />
             </div>
         );

81c91318166160c7b708aed092c8151ddeef9d3b
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-28 12:52:05 +0530

    Added the boiler plate react code.

---
+++ src/Components/common/Box.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Box() {
+    return <div></div>;
+}
+
+export default Box;
---
+++ src/Components/pages/Calendar.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+export class Calendar extends Component {
+    render() {
+        return <div></div>;
+    }
+}
+
+export default Calendar;

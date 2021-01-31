d0f61c006dd37b14e2202bd19ae47ed92653e04f
DeboDevelop <debajyotidofficial@gmail.com>
2021-01-31 18:32:50 +0530

    Fetched the data using axios to display the cards.

--- src/Components/pages/Details.js
+++ src/Components/pages/Details.js
@@ -1,7 +1,9 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useHistory, useLocation } from "react-router-dom";
 
 function Details() {
+    let history = useHistory();
     let query = new URLSearchParams(useLocation().search);
     const [date] = useState(() => {
         return {
@@ -10,17 +12,78 @@ function Details() {
             year: query.get("year"),
         };
     });
-
+    const [state, setState] = useState({
+        posts: [],
+        continuationToken: null,
+        currIndex: 0,
+        startIndex: 0,
+    });
+    useEffect(() => {
+        axios
+            .post("http://quinncareapi-dev.us-east-2.elasticbeanstalk.com/graph", {
+                requestobjects: [
+                    {
+                        posts: {
+                            operationtype: "read",
+                            id: {
+                                return: true,
+                            },
+                            userid: {
+                                searchvalues: ["41329663-5834-11eb-8e6e-3ca82abc3dd4"],
+                                return: true,
+                            },
+                            iscalendarentry: {
+                                searchvalues: ["true"],
+                                return: true,
+                            },
+                            images: {
+                                return: true,
+                            },
+                            rating: {
+                                return: true,
+                            },
+                            text: {
+                                return: true,
+                            },
+                            privacy: {
+                                searchvalues: [18],
+                                return: true,
+                            },
+                            typeofday: {
+                                return: true,
+                            },
+                            calendardatetime: {
+                                return: true,
+                                sort: "descending",
+                            },
+                            maxitemcount: "10",
+                            continuationtoken: state.continuationToken,
+                        },
+                    },
+                ],
+            })
+            .then(res => {
+                setState(() => ({
+                    ...state,
+                    continuationToken: res.data.responseobjects[0].continuationtoken,
+                    posts: [...state.posts, ...res.data.responseobjects[0].posts],
+                }));
+            })
+            .catch(err => console.log(err));
+        // eslint-disable-next-line
+    }, []);
+    console.log("Infinite");
     return (
         <div>
             {date.day === null || date.month === null || date.year === null ? (
-                <h1> No Details</h1>
+                history.push("/")
             ) : (
                 <>
-                    <h1>Details</h1>
-                    <h1>{date.day}</h1>
-                    <h1>{date.month}</h1>
-                    <h1>{date.year}</h1>
+                    {state.posts.length !== 0 ? (
+                        <div>{state.posts[state.currIndex].calendardatetime}</div>
+                    ) : (
+                        <div>Hello</div>
+                    )}
                 </>
             )}
         </div>
